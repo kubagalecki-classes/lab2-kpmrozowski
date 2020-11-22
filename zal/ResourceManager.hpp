d6a231e76d94f7b4c0c6d748ba8c33bef6be1b18
kpmrozowski <kpmrozowski@gmail.com>
2020-11-22 17:44:02 +0000

    zal2_po-terminie_v7

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -16,28 +16,35 @@ public:
         this->res = new Resource{};
     }
     ResourceManager(ResourceManager&obj) {
-        cout << "copy constructor";
+        cout << "copy constructor\n";
         this->res = new Resource{*obj.getResource()};
     }
     ResourceManager(ResourceManager&& obj) {
-        cout << "move constructor";
+        cout << "move constructor\n";
         this->res = obj.getResource();
         obj.res   = (std::nullptr_t) nullptr;
     }
     ResourceManager& operator=(ResourceManager&obj) {
-        cout << "copy assignment operator";
-        this->res = new Resource{*obj.getResource()};
+        cout << "copy assignment operator\n";
+        if (this == &obj) {
+            return *this;
+        }
         delete res;
+        this->res = new Resource{*obj.getResource()};
         return *this;
     }
     ResourceManager& operator=(ResourceManager&&obj) {
-        cout << "move assignment operator";
+        cout << "move assignment operator\n";
+        if (this == &obj) {
+            return *this;
+        }
+        delete res;
         this->res = obj.res;
         obj.res   = (std::nullptr_t) nullptr;
         return *this;
     }
     ~ResourceManager() {
-        cout << "denstructor";
+        cout << "denstructor\n";
         delete res;
     }
     double get() {
